0a2dff49a5d5589cd827c18bfe225f2b4a6b34bf
altafsubhan <ms3altaf@uwaterloo.ca>
2018-09-06 23:17:35 -0400

    down arrow + mouseover

--- js/index.js
+++ js/index.js
@@ -1,5 +1,12 @@
 $(document).ready(() => {
+    var w = window.innerWidth;
+    setTimeout(() => {
+        $('#scroll-down-pointer').css("visibility", "visible");
+        $('#scroll-down-pointer').css("transform", "translateX(" + 0.36*(w) + "px)");
+    }, 1000);
+
     $(window).scroll(() => {
         $("#navbar").css("display", "block");
+        $('#scroll-down-pointer').css("visibility", "hidden");
     });
 });
